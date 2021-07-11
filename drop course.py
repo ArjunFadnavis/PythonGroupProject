e9a4d6a4e1ac880ae42422d495e05bc05e7bd71e
ArjunFadnavis <83542668+ArjunFadnavis@users.noreply.github.com>
2021-07-11 17:04:20 -0400

    Login, and extra credit done, and added to main module

--- drop course.py
+++ drop course.py
@@ -5,7 +5,11 @@ def main():
     course_hours = [3, 4, 5, 3]
     max_size_list = [3, 2, 1, 3]
     roster_list = [['1004', '1003'], ['1001'], ['1002'], []]
-    id = input('Enter ID to login, or 0 to quit: ')
+    lowercase_only = []
+    uppercase_only = []
+    nums_only = []
+    letters_only = []
+    login()
     choice = int(input('Enter 1 to add course, 2 to drop course, 3 to list courses, 4 to show bill, 0 to exit: '))
     while choice != 0:
         if choice == 1:
@@ -76,6 +80,80 @@ def drop_course(id, c_list, r_list):
     else:
         print('course dose not exist')
     # pass # temporarily avoid empty function definition
+
+    # login function
+
+def login():
+    student_id = input('enter student id ')
+    file = open('passwords', 'r+')
+    for line in file:
+        if student_id in line:
+            line = line.strip()
+            # has no password
+            if line == student_id:
+                while True:
+                    try:
+                        pin = (input('enter pin '))
+                        if (student_id, pin) not in student_list:
+                            raise ()
+                    except:
+                        print('error invalid login')
+                        student_id = input('enter student id ')
+                    else:
+                        # sets password
+                        while True:
+                            try:
+                                password = input('enter a password ')
+                                # password strength check
+                                for characters in password:
+                                    if characters.isupper():
+                                        uppercase_only.append(characters)
+                                    if characters.islower():
+                                        lowercase_only.append(characters)
+                                    if characters.isnumeric():
+                                        nums_only.append(characters)
+                                    if characters.isalpha():
+                                        letters_only.append(characters)
+                                    if len(password) < 8:
+                                        print('password must be 8 or more characters long')
+                                        raise ()
+                                    elif len(uppercase_only) == len(password):
+                                        print('passwords must have mixed cases')
+                                        print('jello')
+                                    elif len(lowercase_only) == len(password):
+                                        print('passwords must have mixed cases')
+                                        raise ()
+                                    elif len(nums_only) == len(password):
+                                        print('passwords must have letters')
+                                        raise ()
+                                    elif len(letters_only) == len(password):
+                                        print('passwords must have numbers')
+                                        raise ()
+                                    elif password.isalnum():
+                                        print('passwords must have special characters')
+                                        raise ()
+
+                            except:
+                                continue
+                            else:
+                                line = student_id + ' ' + password
+                                file.write(line)
+                                break
+                        break
+
+            else:
+                # password is set
+                password = input('enter password ')
+                if line == f'{student_id} {password}':
+                    print('valid login')
+                    break
+                else:
+                    # incorrect password
+                    while line != f'{student_id} {password}':
+                        print('invalid login')
+                        student_id = int(input('enter student id '))
+                        password = input('enter password ')
+    file.close()
 
 
 main()
