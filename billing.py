37d38bcabede686f51408f8747f3de8661659be8
Alexandre Eldredge <aweldr@Alexandres-MacBook-Pro.local>
2021-07-14 18:14:16 -0400

    while loop fix, import fix

--- billing.py
+++ billing.py
@@ -1,27 +1,22 @@
-from Registration import course_list
-from Registration import roster_list
-from Registration import course_hours
-
-
 def calculate_hours_and_bill(id, i_s_list, r_list, h_list):
     enrollment_price = 0
     # set tuition prices
-    while str(id) in i_s_list:
+    if str(id) in i_s_list:
         cost = 225
     else:
         cost = 850
-    CSC101 = roster_list[0] # these are lists of the student ids in the class
-    CSC102 = roster_list[1]
-    CSC103 = roster_list[2]
-    CSC104 = roster_list[3]
+    CSC101 = r_list[0] # these are lists of the student ids in the class
+    CSC102 = r_list[1]
+    CSC103 = r_list[2]
+    CSC104 = r_list[3]
     while id in 101:
-        enrollment_price = cost * course_hours[0]
+        enrollment_price = cost * h_list[0]
     while id in 102:
-        enrollment_price = enrollment_price + (cost * course_hours[1])
+        enrollment_price = enrollment_price + (cost * h_list[1])
     while id in 103:
-        enrollment_price = enrollment_price + (cost * course_hours[2])
+        enrollment_price = enrollment_price + (cost * h_list[2])
     while id in 100:
-        enrollment_price = enrollment_price + (cost * course_hours[3])
+        enrollment_price = enrollment_price + (cost * h_list[3])
     print("total cost:", enrollment_price)
     print("course hours registered for", enrollment_price / cost)
     # need to just return the values to the display_hours_bill function
